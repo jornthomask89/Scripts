fb4243f97965b3c0a15ac76954741461b4584c97
jornthomask89 <72712949+jornthomask89@users.noreply.github.com>
2022-11-04 14:57:19 +0100

    Add files via upload

---
+++ WinActivate.cmd
@@ -0,0 +1,13 @@
+@echo off
+FOR /F "skip=1" %%A IN ('wmic path SoftwareLicensingService get OA3xOriginalProductKey') DO  (
+SET "ProductKey=%%A"
+goto InstallKey
+)
+:InstallKey
+IF [%ProductKey%]==[] (
+EXIT /b 1
+) ELSE (
+changepk.exe /ProductKey %ProductKey%
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\FIRMANAVN\FWEK\ /V ActivationStatus /T REG_DWORD /d "1" /F
+EXIT /b 0
+)
