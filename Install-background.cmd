57c7550dc5a51e42482f4da44879dd8c2e109b7c
jornthomask89 <72712949+jornthomask89@users.noreply.github.com>
2022-11-10 15:31:02 +0100

    Update Install-background.cmd

--- Install-background.cmd
+++ Install-background.cmd
@@ -10,13 +10,7 @@ IF %errorlevel%==0 GOTO QUERY
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\ProgramData\CustomScripts\BrakarBackground.png /f 
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Brakar\Background" /V BrakarBackground /T REG_DWORD /d "1" /F
-
-
-
-:QUERY
-REG QUERY HKLM\SOFTWARE\Brakar\Background /v ActivationStatus
-IF %errorlevel%==1 GOTO INSTALL
-IF %errorlevel%==0 GOTO DIE
+GOTO DIE
 
 :DIE
 EXIT /b 1
